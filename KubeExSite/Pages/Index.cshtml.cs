406dd1f5af5285b7f658dec10f5e6367f5dcc3ff
Bawornsit.C <puksigon@gmail.com>
2022-03-14 23:36:24 +0700

    Add: debug

--- KubeExSite/Pages/Index.cshtml.cs
+++ KubeExSite/Pages/Index.cshtml.cs
@@ -10,14 +10,19 @@ namespace KubeExSite.Pages
     {
         private readonly ILogger<IndexModel> _logger;
         private readonly KubeExContext _dbContext;
-        public IndexModel(ILogger<IndexModel> logger, KubeExContext dbContext)
+        private readonly IConfiguration _configuration;
+
+        public IndexModel(ILogger<IndexModel> logger, KubeExContext dbContext, IConfiguration configuration)
         {
             _logger = logger;
             _dbContext = dbContext;
+            _configuration = configuration;
         }
 
         public IActionResult OnGet()
         {
+            _logger.Log(LogLevel.Information,"CheckVaultUrl: "+_configuration.GetValue<string>("KeyVaultUrl"));
+            _logger.Log(LogLevel.Information,"CheckDbConnectionKey: "+_configuration.GetValue<string>("DbConnectionKey"));
             ViewData["routine_reports"] = _dbContext.RoutineReports.AsQueryable().Take(100).ToList();
 
             return Page();
